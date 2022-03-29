69bb874c8dd15477b93863c04206d4c5b6204e9d
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-03-29 11:42:36 +0530

    phalcon blog

--- src/app/controllers/SignupController.php
+++ src/app/controllers/SignupController.php
@@ -1,52 +1,72 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Escaper;
 
 class SignupController extends Controller
 {
     public function indexAction()
     {
+        $escaper = new Escaper();
         $user = new Users();
-        if($this->request->getPost()){
-        $this->view->users = Users::find();
-        $user->assign(
-            $postdata = $this->request->getPost(),
-            [
-                'full_name',
-                'username',
-                'email',
-                'password',
-                'confirm_password',
+        if ($this->request->getPost()) {
+            $this->view->users = Users::find();
+            $postdata=array(
+                'full_name'=>$escaper->escapehtml($this->request->getPost('full_name')),
+                'username'=>$escaper->escapehtml($this->request->getPost('username')),
+                'email'=>$escaper->escapehtml($this->request->getPost('email')),
+                'password'=>$escaper->escapehtml($this->request->getPost('password')),
+                'confirm_password'=>$escaper->escapehtml($this->request->getPost('confirm_password')),
 
-            ]
-        );
-        $user->role = 'user';
-        $user->status = 'pending';
-        // $emailExists = $this->view->users = Users::find($postdata['email']);
-        if($postdata['password']!=$postdata['confirm_password'])
-        {
-        $this->view->passwordError = "Passwords don't match"."<br>";
-        }
-        // foreach($user as $k){
-        //     if($k->email == $postdata['email']){
-        //         $this->view->emailExists = "<br>"."Email already exists"."<br>";
-        //     }
-            
-        //     }
+            );
+            $user->assign(
+                $postdata,
+                [
+                    'full_name',
+                    'username',
+                    'email',
+                    'password',
+                    'confirm_password',
 
-        
+                ]
+            );
+            $user->role = 'user';
+            $user->status = 'pending';
+            // function emailExists($email)
+            // {
+            //     $user = new Users();
+            //     foreach ($user as $k) {
+            //         if ($k->email == $email) {
+            //             return true;
+            //         } else {
+            //             return false;
+            //         }
+            //     }
+            // }
+            // $emailExists = emailExists($postdata->email);
+            // $emailExists = $this->view->users = Users::find($postdata['email']);
+            if ($postdata['password'] != $postdata['confirm_password']) {
+                $this->view->passwordError = "Passwords don't match" . "<br>";
+            }
+            // foreach($user as $k){
+            //     if($k->email == $postdata['email']){
+            //         $this->view->emailExists = "<br>"."Email already exists"."<br>";
+            //     }
 
-        else{
-        $success = $user->save();
-        $this->view->success = $success;
+            //     }
+
+            // if ($emailExists) {
+            //     $this->view->emailExistsError = "*Email already Registered !! Try with a different email !!";
+            // } 
+            else {
+                $success = $user->save();
+                $this->view->success = $success;
+            }
+            if ($success) {
+                $this->view->message = "Register succesfully";
+            } else {
+                $this->view->message = "Not Register succesfully due to following reason: <br>" . implode("<br>", $user->getMessages());
+            }
         }
-        if ($success) {
-            $this->view->message = "Register succesfully";
-        } else {
-            $this->view->message = "Not Register succesfully due to following reason: <br>" . implode("<br>", $user->getMessages());
-        }
-    }}
-    public function testAction()
-    {
     }
 }
